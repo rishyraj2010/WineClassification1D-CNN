01ba62c648730e231831bef594c04d3534150468
rishyraj2010 <44636310+rishyraj2010@users.noreply.github.com>
2023-12-26 01:07:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,36 @@
+from flask import Flask,request,render_template,url_for
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib as joblib
+import os
 
-from flask import Flask, request, jsonify, render_template
+model=joblib.load('iris_model_LR.pkl')
+scaler=joblib.load('scaler.save')
 
-import tensorflow as tf
+app =Flask(__name__)
 
-# Assuming you saved the model using model.save
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    import numpy as np
-from flask import Flask, render_template, request
-import tensorflow as tf
-
-app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Assuming the form fields contain features as input
-    flt_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(flt_features)]
-    
-    # Load the model
-    model = tf.keras.models.load_model('white_wine_pipeline_1DCNN.keras')
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method =='POST':
+        model = tf.keras.models.load_model('white_wine_pipeline_1DCNN.keras')
+        output = np.argmax(model.predict([[[ 1.57912294],
+       [ 1.32836936],
+       [ 0.27590734],
+       [ 1.05049255],
+       [-1.14934812],
+       [-0.30195909],
+       [-0.28255704],
+       [ 0.28507364],
+       [-1.11445795],
+       [ 0.08897337],
+       [-0.28021359]]]), axis=1)
 
-    # Predict using the loaded model
-    output = np.argmax(model.predict(np.array(final_features)), axis=1)
-
-    return render_template('index.html', prediction_text='Wine Quality is $ {}'.format(output[0]))
+    return render_template('index.html', prediction_text='Wine Quality is $ {}'.format(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
