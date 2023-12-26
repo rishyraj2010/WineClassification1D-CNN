07483e061fbf4282fc658b2d550a4070aff18a1c
rishyraj2010 <44636310+rishyraj2010@users.noreply.github.com>
2023-12-26 00:08:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict():
     flt_features = [float(x) for x in request.form.values()]
     final_features = [np.array(flt_features)]
     model = tf.keras.models.load_model('white_wine_pipeline_1DCNN.keras')
-    output = np.argmax(model.predict([[[final_features[x]] for x in final_features]), axis=1)
+    output = np.argmax(model.predict(([final_features[x]] for x in final_features), axis=1)
 
     return render_template('index.html', prediction_text='Wine Quality is $ {}'.format(output))
 
