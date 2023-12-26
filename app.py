efe3100856e4e678213ccef6b86894a16ddb672f
rishyraj2010 <44636310+rishyraj2010@users.noreply.github.com>
2023-12-26 00:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,16 +15,29 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+    import numpy as np
+from flask import Flask, render_template, request
+import tensorflow as tf
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Assuming the form fields contain features as input
     flt_features = [float(x) for x in request.form.values()]
     final_features = [np.array(flt_features)]
+    
+    # Load the model
     model = tf.keras.models.load_model('white_wine_pipeline_1DCNN.keras')
-    output = np.argmax(model.predict(([final_features[x]] for x in final_features), axis=1)
 
-    return render_template('index.html', prediction_text='Wine Quality is $ {}'.format(output))
+    # Predict using the loaded model
+    output = np.argmax(model.predict(np.array(final_features)), axis=1)
 
+    return render_template('index.html', prediction_text='Wine Quality is $ {}'.format(output[0]))
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
